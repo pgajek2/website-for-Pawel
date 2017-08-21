e05dfbbb9e902349c2e68dd584d39ac16704d238
unknown <pgajek2@gmail.com>
2017-08-21 13:43:18 +0200

    version 1.1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var flag = false;
+function showMenu(){
+    if(!flag){
+        document.getElementById("current-menu").style.display = "block";
+        flag = !flag;
+    } else {
+        document.getElementById("current-menu").style.display = "none";
+        flag = !flag;
+    }
+}
